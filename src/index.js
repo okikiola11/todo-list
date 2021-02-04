f2bffb1e98642fd4b1055d9d10fd79bea215161e
okikiola11 <okikiola.apelehin@gmail.com>
2021-02-04 14:42:55 +0000

    Add todo project name Dynamically display todo item

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import './styles/index.css';
 
-const addTask = document.querySelector('#task-input').value;
+const todoInput = document.querySelector('.task-input');
+const todoList = document.querySelector('.todo-list');
+const todoButton = document.querySelector('.todo-button');
+const taskInfo = document.querySelector('.task-info');
+const newTodo = document.createElement('li');
 
-function showDiv() {
-  console.log('hello');
-  addTask.value;
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', getSingleTodoItem);
+
+// Functions
+
+function addTodo(event) {
+  event.preventDefault();
+
+  // Todo DIV
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  
+  // Create LI
+  
+  newTodo.innerText = todoInput.value;
+  console.log(newTodo);
+  newTodo.classList.add('todo-item');
+
+  todoDiv.appendChild(newTodo);
+  todoList.appendChild(todoDiv);
+  
+  // Clear todo Input value
+  todoInput.value = "";
 }
 
-showDiv();
+function getSingleTodoItem(e) {
+  const show = document.createElement('div');
+  show.appendChild(newTodo);
+
+  taskInfo.appendChild(show);
+
+}
